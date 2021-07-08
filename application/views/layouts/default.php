5ad6b42cba6e60a36e021556442881e90c7e26db
Stanislav Ryndin <slava.jax@gmail.com>
2021-07-08 20:28:05 +0300

    Добавление общего шаблона и формы на главной

--- application/views/layouts/default.php
+++ application/views/layouts/default.php
@@ -1,13 +1,13 @@
 <!doctype html>
 <html lang="ru">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title><?=$title?></title>
 </head>
 <body>
-    <?php echo $content;?>
+<?=$content?>
 </body>
 </html>
--- application/views/main/index.php
+++ application/views/main/index.php
@@ -1,2 +1,27 @@
-<h1>Главная страница</h1>
-<p>Абзац</p>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 mt-3">
+            <form action="/" method="post">
+                <div class="mb-3">
+                    <label for="inputName" class="form-label">Имя<sup>*</sup>:</label>
+                    <input type="text" class="form-control" id="inputName" name="name" aria-describedby="nameHelp">
+                    <div id="nameHelp" class="form-text">Это поле обязательно для заполнения</div>
+                </div>
+                <div class="mb-3">
+                    <label for="inputSurname" class="form-label">Фамилия:</label>
+                    <input type="text" class="form-control" id="inputSurname" name="surname" aria-describedby="surnameHelp">
+                </div>
+                <div class="mb-3">
+                    <label for="inputEmail" class="form-label">Email<sup>*</sup>:</label>
+                    <input type="email" class="form-control" id="inputEmail" name="email" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text">Это поле обязательно для заполнения</div>
+                </div>
+                <div class="mb-3">
+                    <label for="inputMessage" class="form-label">Текст сообщения</label>
+                    <textarea class="form-control" id="inputMessage" rows="3" name="text"></textarea>
+                </div>
+                <input type="submit" class="btn btn-primary" value="Отправить">
+            </form>
+        </div>
+    </div>
+</div>
